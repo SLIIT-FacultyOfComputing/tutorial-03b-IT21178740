288bd775c97d4b809e8ad5487b36e1f5cf59f52c
IT21178740 <99183786+IT21178740@users.noreply.github.com>
2022-03-23 13:00:07 +0000

    i completed exercise 1 in tutorial 3

--- exercise01.cpp
+++ exercise01.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main() {
  float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
  char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
- cout <<setw(5)<<"No" <<setw(15)<< "Name" <<setw(10)<< "Marks" << endl;
+ cout <<setw(5)<<"No" <<setw(20)<< "Name" <<setw(25)<< "Marks" << endl;
  for (int r = 0; r < 5; r++) {
      cout << setw(5)<< r+1 
-         <<setw(15) <<  names[r]
-         <<setw(10)<<setprecision(2)
+         <<setw(20) <<  names[r]
+         <<setw(25)<<setprecision(2)
          <<setiosflags(ios::fixed) << marks[r] << endl;
  }
   return 0;
